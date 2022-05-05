82928835305ecf696c3d3a394a6b3cabb5dd1db0
amine tayani <amine.tayanipro@gmail.com>
2022-05-05 13:57:38 +0000

    fix : change section layout

--- src/components/Sections.tsx
+++ src/components/Sections.tsx
@@ -14,10 +14,11 @@ const Sections: React.FunctionComponent = () => {
               about your job opportunities regardless of where you found them
             </p>
           </div>
-          <div className="relative mt-8 flex h-96 w-full overflow-hidden rounded-xl bg-blue-400 bg-gradient-to-r from-[#0052D4] via-[#4364F7] to-[#6FB1FC] p-20 sm:mr-10 md:w-1/2 lg:left-32 lg:mt-0 lg:h-[600px] lg:w-3/5">
-            <div className="flex flex-wrap py-6 ">
-              <div className="px-6">
+          <div className="relative mt-8 flex h-96 w-full items-center justify-center overflow-hidden rounded-xl bg-blue-400 bg-gradient-to-r from-[#0052D4] via-[#4364F7] to-[#6FB1FC] sm:mr-10 md:w-1/2 lg:left-40 lg:mt-0 lg:h-[600px] lg:w-3/5 lg:p-20">
+            <div className="flex flex-wrap ">
+              <div className="">
                 <img
+                  className=" relative max-w-[300px] rounded-2xl lg:left-16 lg:max-w-3xl"
                   src="https://res.cloudinary.com/glasnik/image/upload/v1651756076/media/image2-section_tefzfp.png"
                   alt=""
                 />
@@ -28,10 +29,11 @@ const Sections: React.FunctionComponent = () => {
       </section>
       <section className="body-font relative text-gray-600">
         <div className="container mx-auto flex flex-wrap items-center px-5 py-24 sm:flex-nowrap">
-          <div className="relative order-last flex h-96 w-full overflow-hidden rounded-xl bg-blue-400 bg-gradient-to-r from-[#0052D4] via-[#4364F7] to-[#6FB1FC] p-20 sm:mr-10 md:w-1/2 lg:right-32 lg:order-first lg:h-[600px] lg:w-3/5">
-            <div className="mt-8 flex flex-wrap py-6">
-              <div className="px-6">
+          <div className="relative order-last flex h-96 w-full justify-center overflow-hidden rounded-xl bg-blue-400 bg-gradient-to-r from-[#0052D4] via-[#4364F7] to-[#6FB1FC] p-20 sm:mr-10 md:w-1/2 lg:right-40 lg:order-first lg:h-[600px] lg:w-3/5">
+            <div className="flex flex-wrap ">
+              <div className="">
                 <img
+                  className="relative max-w-[300px] rounded-2xl lg:right-12 lg:max-w-3xl"
                   src="https://res.cloudinary.com/glasnik/image/upload/v1651756079/media/image1-section_v5ghrn.png"
                   alt=""
                 />
